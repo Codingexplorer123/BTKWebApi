c0f57a3b7df79a0d37b8b26b2d7990e3d27122a6
Codingexplorer123 <berkozgurca@gmail.com>
2024-08-28 21:55:25 +0300

    BooksController action methodlari yazildi.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace WebApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddNewtonsoftJson();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
