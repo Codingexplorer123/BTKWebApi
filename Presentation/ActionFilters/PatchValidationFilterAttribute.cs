a251a27c6b243222114b08110a947934c96255d1
Codingexplorer123 <berkozgurca@gmail.com>
2024-09-08 13:49:59 +0300

    API ActionFilterlar eklendi

---
+++ Presentation/ActionFilters/PatchValidationFilterAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.ActionFilters
+{
+    public class PatchValidationFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = context.RouteData.Values["controller"];
+            var action = context.RouteData.Values["action"];
+
+            var patchDocument = context.ActionArguments.Values.FirstOrDefault(v=>v is Microsoft.AspNetCore.JsonPatch.JsonPatchDocument);
+
+            if (patchDocument == null)
+            {
+                context.Result = new UnprocessableEntityObjectResult($"object is null." + $"Controller: {controller}, Action: {action}");
+
+                return;
+            }
+
+          
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NLog;
+using Presentation.ActionFilters;
 using Repositories.EFCore;
 using Services.Contracts;
 using WebApi.Extensions;
@@ -19,6 +20,8 @@ builder.Services.AddControllers(config =>
     .AddApplicationPart(typeof(Presentation.AssemblyRefence).Assembly)
     .AddNewtonsoftJson();
 
+
+
 builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -28,6 +31,8 @@ builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureLoggerService();
 builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.ConfigureActionFilters();
 
 
 var app = builder.Build();
